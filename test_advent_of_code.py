04828cb63068ec572f166dffd490c23f597b34f0
Holly Becker <becker.holly@gmail.com>
2015-12-03 00:10:52 -0800

    Advent of Code: Day 2

--- test_advent_of_code.py
+++ test_advent_of_code.py
@@ -2,7 +2,7 @@
 Tests for Advent of Code
 """
 
-import day1
+import day1, day2
 
 
 def test_day_1():
@@ -19,3 +19,13 @@ def test_day_1():
 
     assert day1.basement_entrance(')') == 1
     assert day1.basement_entrance('()())') == 5
+
+def test_day_2():
+    assert day2.order_paper(['2x3x4']) == 58
+    assert day2.order_paper(['1x1x10']) == 43
+    assert day2.order_paper(['2x3x4', '1x1x10']) == 58 + 43
+
+    assert day2.order_ribbons(['2x3x4']) == 34
+    assert day2.order_ribbons(['1x1x10']) == 14
+    assert day2.order_ribbons(['2x3x4', '1x1x10']) == 34 + 14
+
