64fd40698ce71b3e77a7e3f1081b58e6acfde137
Holly Becker <becker.holly@gmail.com>
2015-12-17 00:09:54 -0800

    Advent of Code: Day 10 fixup description

--- day10.py
+++ day10.py
@@ -17,6 +17,12 @@ For example:
 
 Starting with the digits in your puzzle input, apply this process 40 times. What is the length of the result?
 
+--- Part Two ---
+
+Neat, right? You might also enjoy hearing John Conway talking about this sequence (that's Conway of Conway's Game of Life fame).
+
+Now, starting again with the digits in your puzzle input, apply this process 50 times. What is the length of the new result?
+
 """
 import itertools
 
@@ -42,3 +48,4 @@ def look_and_say(data, iterations=1, version=version2):
 if __name__ == '__main__':
     data = '1113122113'
     print("Look and say after 40 iterations", len(look_and_say(data, 40)))
+    print("Look and say after 50 iterations", len(look_and_say(data, 50)))
