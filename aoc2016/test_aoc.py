e8d0e9508bfc14eca73d5286033d5058669a0653
Holly Becker <becker.holly@gmail.com>
2016-11-30 22:26:44 -0800

    Day 1: timed pass

---
+++ aoc2016/test_aoc.py
@@ -0,0 +1,11 @@
+"""
+Tests for Advent of Code
+"""
+import day1
+
+def test_day_1():
+    assert day1.solve('R2, L3') == 5
+    assert day1.solve('R2, R2, R2') == 2
+    assert day1.solve('R5, L5, R5, R3') == 12
+
+    assert day1.solve2('R8, R4, R4, R8') == 4
