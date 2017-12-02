75fc12c2d5ddbecc592c2760a2f387b8ad1f999f
Holly Becker <becker.holly@gmail.com>
2017-12-01 21:18:12 -0800

    2017 Day 2 solution

--- aoc2017/test_aoc.py
+++ aoc2017/test_aoc.py
@@ -5,7 +5,7 @@ import unittest
 
 import pytest
 
-from . import day1
+from . import day1, day2
 
 @pytest.mark.parametrize('seq,sum,halfway', [
     ('1122', 3, False),
@@ -20,3 +20,14 @@ from . import day1
 ])
 def test_day_1(seq, sum, halfway):
     assert day1.solve(seq, halfway=halfway) == sum
+
+@pytest.mark.parametrize('sheet,checksum,modulo', [
+    ('''5 1 9 5
+7 5 3
+2 4 6 8''', 18, False),
+    ('''5 9 2 8
+9 4 7 3
+3 8 6 5''', 9, True),
+])
+def test_day_2(sheet, checksum,modulo):
+    assert day2.solve(sheet, modulo=modulo) == checksum
