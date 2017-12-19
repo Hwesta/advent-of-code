f2abeb7b8e1d4f16f0dc80f3b0c0f9d0ab341878
Holly Becker <becker.holly@gmail.com>
2017-12-18 23:16:58 -0800

    2017 day18 part 2 WIP

--- aoc2017/day18.py
+++ aoc2017/day18.py
@@ -42,25 +42,52 @@ At the time the recover operation is executed, the frequency of the last sound p
 
 What is the value of the recovered frequency (the value of the most recently played sound) the first time a rcv instruction is executed with a non-zero value?
 
+--- Part Two ---
+
+As you congratulate yourself for a job well done, you notice that the documentation has been on the back of the tablet this entire time. While you actually got most of the instructions correct, there are a few key differences. This assembly code isn't about sound at all - it's meant to be run twice at the same time.
+
+Each running copy of the program has its own set of registers and follows the code independently - in fact, the programs don't even necessarily run at the same speed. To coordinate, they use the send (snd) and receive (rcv) instructions:
+
+    snd X sends the value of X to the other program. These values wait in a queue until that program is ready to receive them. Each program has its own message queue, so a program can never receive a message it sent.
+    rcv X receives the next value and stores it in register X. If no values are in the queue, the program waits for a value to be sent to it. Programs do not continue to the next instruction until they have received a value. Values are received in the order they are sent.
+
+Each program also has its own program ID (one 0 and the other 1); the register p should begin with this value.
+
+For example:
+
+snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d
+
+Both programs begin by sending three values to the other. Program 0 sends 1, 2, 0; program 1 sends 1, 2, 1. Then, each program receives a value (both 1) and stores it in a, receives another value (both 2) and stores it in b, and then each receives the program ID of the other program (program 0 receives 1; program 1 receives 0) and stores it in c. Each program now sees a different value in its own copy of register c.
+
+Finally, both programs try to rcv a fourth time, but no data is waiting for either of them, and they reach a deadlock. When this happens, both programs terminate.
+
+It should be noted that it would be equally valid for the programs to run at different speeds; for example, program 0 might have sent all three values and then stopped at the first rcv before program 1 executed even its first instruction.
+
+Once both of your programs have terminated (regardless of what caused them to do so), how many times did program 1 send a value?
+
 """
 from __future__ import print_function
+import asyncio
 import os
 from collections import defaultdict
 
-def solve(data, flag=False):
-    registers = defaultdict(int)
-
-    pc = 0
-    instructions = data.splitlines()
-    last_sound = None
-
+def compute(instructions, registers, pc, input_buffer, output_buffer):
     def get_value(x):
         try:
             return int(x)
-        except:  # Must be a register
+        except ValueError:  # Must be a register
             return registers[x]
-
+    # print('input', input_buffer)
+    # print('output_buffer', output_buffer)
     while pc < len(instructions):
+        # print('registers', registers)
+        # print('pc', pc, instructions[pc])
         row = instructions[pc].split()
         action = row[0]
         if action == 'set':
@@ -69,21 +96,74 @@ def solve(data, flag=False):
             registers[row[1]] += get_value(row[2])
         elif action == 'mul':
             registers[row[1]] *= get_value(row[2])
-        elif action =='mod':
+        elif action == 'mod':
             registers[row[1]] %= get_value(row[2])
         elif action == 'jgz':
             if get_value(row[1]) != 0:
                 pc += get_value(row[2])
                 pc -= 1   # Compensate for adding to it later
         elif action == 'snd':
-            last_sound = registers[row[1]]
+            output_buffer.append(registers[row[1]])
+            # print('output_buffer', output_buffer)
         elif action == 'rcv':
-            if get_value(row[1]) != 0:
-                break
+            if input_buffer:
+                registers[row[1]] = input_buffer.pop(0)
+            else:
+                # Return state?
+                return registers, pc, output_buffer
 
         pc += 1
 
-    return last_sound
+def solve(data, flag=False):
+    instructions = data.splitlines()
+    registers = defaultdict(int)
+    pc = 0
+    input_buffer = []
+    output_buffer = []
+    print('instructions', instructions)
+
+    if not flag:
+        _, _, output_buffer = compute(
+            instructions, registers, pc, input_buffer, output_buffer)
+        return output_buffer[0]
+
+    # Run until one runs out of input buffer
+    # Run the other
+    # While A's output buffer is empty?
+    registers['p'] = 0
+
+    registers2 = defaultdict(int)
+    registers2['p'] = 1
+    pc2 = 0
+    output_buffer2 = []
+
+    i = 0
+    sentcount = 0
+    while True:
+        # print('RUN PROG 1')
+        registers, pc, output_buffer = compute(
+            instructions, registers, pc, output_buffer2, output_buffer)
+        # print('output_buffer', output_buffer)
+
+        print('bufferlens', len(output_buffer), len(output_buffer2))
+        print('buffers', output_buffer, output_buffer2)
+        if not output_buffer and not output_buffer2:
+            break
+
+        # print("RUN PROG 2")
+        registers2, pc2, output_buffer2 = compute(
+            instructions, registers2, pc2, output_buffer, output_buffer2)
+        sentcount += len(output_buffer2)
+        print('sentcount', sentcount)
+        print('bufferlens', len(output_buffer), len(output_buffer2))
+
+        print('buffers', output_buffer, output_buffer2)
+        if not output_buffer and not output_buffer2:
+            break
+
+    # 2050796 too high
+    # 127 too low
+    return sentcount
 
 
 if __name__ == '__main__':
@@ -91,4 +171,4 @@ if __name__ == '__main__':
     with open(os.path.join(this_dir, 'day18.input')) as f:
         data = f.read().strip()
     print(solve(data, False))
-    # print(solve(data, True))
+    print(solve(data, True))
