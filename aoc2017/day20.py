0219efecc9bb012cbad29aef6e5a268fcf60c07a
Holly Becker <becker.holly@gmail.com>
2017-12-21 22:47:11 -0800

    2017 day20 part 1 solution

---
+++ aoc2017/day20.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+"""
+--- Day 20: Particle Swarm ---
+
+Suddenly, the GPU contacts you, asking for help. Someone has asked it to simulate too many particles, and it won't be able to finish them all in time to render the next frame at this rate.
+
+It transmits to you a buffer (your puzzle input) listing each particle in order (starting with particle 0, then particle 1, particle 2, and so on). For each particle, it provides the X, Y, and Z coordinates for the particle's position (p), velocity (v), and acceleration (a), each in the format <X,Y,Z>.
+
+Each tick, all particles are updated simultaneously. A particle's properties are updated in the following order:
+
+    Increase the X velocity by the X acceleration.
+    Increase the Y velocity by the Y acceleration.
+    Increase the Z velocity by the Z acceleration.
+    Increase the X position by the X velocity.
+    Increase the Y position by the Y velocity.
+    Increase the Z position by the Z velocity.
+
+Because of seemingly tenuous rationale involving z-buffering, the GPU would like to know which particle will stay closest to position <0,0,0> in the long term. Measure this using the Manhattan distance, which in this situation is simply the sum of the absolute values of a particle's X, Y, and Z position.
+
+For example, suppose you are only given two particles, both of which stay entirely on the X-axis (for simplicity). Drawing the current states of particles 0 and 1 (in that order) with an adjacent a number line and diagram of current X positions (marked in parenthesis), the following would take place:
+
+p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>    -4 -3 -2 -1  0  1  2  3  4
+p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>                         (0)(1)
+
+p=< 4,0,0>, v=< 1,0,0>, a=<-1,0,0>    -4 -3 -2 -1  0  1  2  3  4
+p=< 2,0,0>, v=<-2,0,0>, a=<-2,0,0>                      (1)   (0)
+
+p=< 4,0,0>, v=< 0,0,0>, a=<-1,0,0>    -4 -3 -2 -1  0  1  2  3  4
+p=<-2,0,0>, v=<-4,0,0>, a=<-2,0,0>          (1)               (0)
+
+p=< 3,0,0>, v=<-1,0,0>, a=<-1,0,0>    -4 -3 -2 -1  0  1  2  3  4
+p=<-8,0,0>, v=<-6,0,0>, a=<-2,0,0>                         (0)
+
+At this point, particle 1 will never be closer to <0,0,0> than particle 0, and so, in the long run, particle 0 will stay closest.
+
+Which particle will stay closest to position <0,0,0> in the long term?
+
+--- Part Two ---
+
+To simplify the problem further, the GPU would like to remove any particles that collide. Particles collide if their positions ever exactly match. Because particles are updated simultaneously, more than two particles can collide at the same time and place. Once particles collide, they are removed and cannot collide with anything else after that tick.
+
+For example:
+
+p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>
+p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>    -6 -5 -4 -3 -2 -1  0  1  2  3
+p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>    (0)   (1)   (2)            (3)
+p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>
+
+p=<-3,0,0>, v=< 3,0,0>, a=< 0,0,0>
+p=<-2,0,0>, v=< 2,0,0>, a=< 0,0,0>    -6 -5 -4 -3 -2 -1  0  1  2  3
+p=<-1,0,0>, v=< 1,0,0>, a=< 0,0,0>             (0)(1)(2)      (3)
+p=< 2,0,0>, v=<-1,0,0>, a=< 0,0,0>
+
+p=< 0,0,0>, v=< 3,0,0>, a=< 0,0,0>
+p=< 0,0,0>, v=< 2,0,0>, a=< 0,0,0>    -6 -5 -4 -3 -2 -1  0  1  2  3
+p=< 0,0,0>, v=< 1,0,0>, a=< 0,0,0>                       X (3)
+p=< 1,0,0>, v=<-1,0,0>, a=< 0,0,0>
+
+------destroyed by collision------
+------destroyed by collision------    -6 -5 -4 -3 -2 -1  0  1  2  3
+------destroyed by collision------                      (3)
+p=< 0,0,0>, v=<-1,0,0>, a=< 0,0,0>
+
+In this example, particles 0, 1, and 2 are simultaneously destroyed at the time and place marked X. On the next tick, particle 3 passes through unharmed.
+
+How many particles are left after all collisions are resolved?
+
+"""
+from __future__ import print_function
+import os
+import re
+
+def solve(data, flag=False):
+    particles = []
+    regex = r'p=<([-\d, ]+)>, v=<([-\d, ]+)>, a=<([-\d, ]+)>'
+
+    for i, row in enumerate(data.splitlines()):
+        m = re.match(regex, row)
+        position, velocity, acceleration = m.groups()
+        particles.append({
+            'acceleration': [int(x) for x in acceleration.split(',')],
+            'velocity': [int(x) for x in velocity.split(',')],
+            'position': [int(x) for x in position.split(',')],
+        })
+
+    for i in range(10000):
+        same_spot = {}
+        for p, particle in enumerate(particles):
+            for dimension in range(3):
+                particle['velocity'][dimension] += particle['acceleration'][dimension]
+                particle['position'][dimension] += particle['velocity'][dimension]
+        if i % 1000 == 0:
+            print(i)
+
+    min_distance = 100000000000
+    min_particle = None
+    for p, particle in enumerate(particles):
+        distance = sum(abs(x) for x in particle['position'])
+        if distance < min_distance:
+            min_distance = distance
+            min_particle = p
+
+    # p2: 344 too low, 931 too high
+    return min_particle
+
+
+if __name__ == '__main__':
+    this_dir = os.path.dirname(__file__)
+    with open(os.path.join(this_dir, 'day20.input')) as f:
+        data = f.read().strip()
+    print(solve(data, False))
+    # print(solve(data, True))
