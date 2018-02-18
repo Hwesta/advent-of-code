1d9be780d723a607f9917ac03a317a2dae3b1828
Holly Becker <becker.holly@gmail.com>
2018-02-18 13:17:01 -0800

    2015 day 5 in Ruby

---
+++ aoc2015/day5.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+def solve(data)
+  data.each_line.count do |line|
+    line.count('aeiou') >= 3 &&
+      /(.)\1/.match(line) &&
+      !line.include?('ab') &&
+      !line.include?('cd') &&
+      !line.include?('pq') &&
+      !line.include?('xy')
+  end
+end
+
+def solve2(data)
+  data.each_line.count do |line|
+    /(..).*\1/.match(line) && /(.).\1/.match(line)
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  data = IO.read('day5.input')
+
+  puts "#{solve(data)} strings are nice."
+  puts "#{solve2(data)} strings are nice under the better rules."
+end
