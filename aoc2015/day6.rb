d634640ba362901c550c57e09b5f66d5ca68fa44
Holly Becker <becker.holly@gmail.com>
2018-02-18 20:49:30 -0800

    2015 day 6 in Ruby, convert to array

--- aoc2015/day6.rb
+++ aoc2015/day6.rb
@@ -9,19 +9,21 @@ def update_light(value, action)
 end
 
 def solve(data)
-  lights = Hash.new(false)
+  lights = []
+  (0..999).each do |i|
+    lights[i] = [false] * 1000
+  end
 
   data.each_line do |line|
-    # puts line
     action, ux, uy, lx, ly = /(.*) (\d+),(\d+) through (\d+),(\d+)/.match(
       line
     )[1..5]
 
-    (ux..lx).to_a.product((uy..ly).to_a) do |x, y|
-      lights[[x, y]] = update_light(lights[[x, y]], action)
+    (ux.to_i..lx.to_i).to_a.product((uy.to_i..ly.to_i).to_a) do |x, y|
+      lights[x][y] = update_light(lights[x][y], action)
     end
   end
-  lights.each_value.count(true)
+  lights.flatten.count(true)
 end
 
 def update_light2(value, action)
@@ -34,19 +36,21 @@ end
 
 
 def solve2(data)
-  lights = Hash.new(0)
+  lights = []
+  (0..999).each do |i|
+    lights[i] = [0] * 1000
+  end
 
   data.each_line do |line|
-    # puts line
     action, ux, uy, lx, ly = /(.*) (\d+),(\d+) through (\d+),(\d+)/.match(
       line
     )[1..5]
 
-    (ux..lx).to_a.product((uy..ly).to_a) do |x, y|
-      lights[[x, y]] = update_light2(lights[[x, y]], action)
+    (ux.to_i..lx.to_i).to_a.product((uy.to_i..ly.to_i).to_a) do |x, y|
+      lights[x][y] = update_light2(lights[x][y], action)
     end
   end
-  lights.each_value.sum
+  lights.flatten.sum
 end
 
 if $PROGRAM_NAME == __FILE__
