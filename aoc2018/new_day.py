0ccf0753d227cffe7848b1df276314cfb36956d7
Holly Becker <becker.holly@gmail.com>
2018-12-01 10:54:16 -0800

    2018: Setup

---
+++ aoc2018/new_day.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import shutil
+import stat
+import sys
+import os
+
+day = sys.argv[1]
+dayfile = f'day{day}.py'
+
+# Copy template
+shutil.copy('template.py', dayfile)
+with open(dayfile, 'r') as f:
+    contents = f.read()
+contents = contents.replace('day##', f'day{day}')
+with open(dayfile, 'w') as f:
+    f.write(contents)
+
+# Chmod
+st = os.stat(dayfile)
+os.chmod(dayfile, st.st_mode | stat.S_IEXEC)
+
+# Add test
+with open('test_aoc.py', 'a') as f:
+    f.write(f'''
+@pytest.mark.parametrize('data,answer,flag', [
+
+])
+def test_day_{day}(data, answer, flag):
+    assert day{day}.solve(data, flag) == answer
+''')
+
+# Create input file
+with open(f'day{day}.input', 'a') as f:
+    os.utime(f'day{day}.input', None)
---
+++ aoc2018/template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+
+
+"""
+from __future__ import print_function
+import os
+
+
+def solve(data, flag=False):
+    
+
+    return None
+
+
+if __name__ == "__main__":
+    this_dir = os.path.dirname(__file__)
+    with open(os.path.join(this_dir, "day##.input")) as f:
+        data = f.read().strip()
+    print(solve(data, False))
+    # print(solve(data, True))
