6d7883746190de8d959ec573eb28ef556814e6f8
Holly Becker <becker.holly@gmail.com>
2020-12-06 20:41:54 -0800

    2020 day 1 solution

--- aoc2020/test_aoc.py
+++ aoc2020/test_aoc.py
@@ -1,6 +1,9 @@
+import pytest
+
+from . import day1
 
 @pytest.mark.parametrize('data,answer,flag', [
-
+    (['1721', '979', '366', '299', '675', '1456'], 514579, False),
 ])
 def test_day_1(data, answer, flag):
     assert day1.solve(data, flag) == answer
