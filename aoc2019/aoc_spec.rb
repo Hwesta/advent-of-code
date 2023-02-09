eef5d53094bd1e2756e1e2e53b35c87b370751b3
Holly Becker <becker.holly@gmail.com>
2023-02-08 17:02:40 -0800

    WIP ruby files & day3

--- aoc2019/aoc_spec.rb
+++ aoc2019/aoc_spec.rb
@@ -2,6 +2,7 @@
 
 require_relative 'day1'
 require_relative 'day2'
+require_relative 'day3'
 
 RSpec.describe 'Advent of Code' do
   describe 'Day 1' do
@@ -54,6 +55,23 @@ RSpec.describe 'Advent of Code' do
       end
     end
     context 'part 2' do
+    end
+  end
+
+  describe 'Day 3' do
+    context 'part 1' do
+      where(:input, :answer) do
+        [
+          [["R8,U5,L5,D3", "U7,R6,D4,L4"], 6],
+          [["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"], 159],
+          [["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"], 135],
+        ]
+      end
+      with_them do
+        it do
+          expect(Day3.solve(input)).to eq answer
+        end
+      end
     end
   end
 end
